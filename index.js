189f703e77a53ede4e5802ab35f56d4f9f03d632
Mario Ramirez <ramirezmario@outlook.com>
2015-01-31 20:50:40 -0300

    Handlers

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 var server = require("./server.js");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers.js");
 
-server.iniciar(router.route);
+var handle = {}
+handle["/"] = requestHandlers.iniciar;
+handle["/iniciar"] = requestHandlers.iniciar;
+handle["/subir"] = requestHandlers.subir;
+
+server.iniciar(router.route, handle);
--- router.js
+++ router.js
@@ -1,5 +1,13 @@
-function route(pathname){
-	console.log("A punto de rutear una petición para " + pathname);
+function route(handle, pathname, response) {
+  console.log("A punto de rutear una peticion para " + pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname](response);
+  } else {
+    console.log("No hay manipulador de peticion para " + pathname);
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("404 No Encontrado");
+    response.end();
+  }
 }
 
 exports.route = route;
